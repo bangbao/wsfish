1c9be4b960f218bff381afff3bab99c7355e212b
zyw <hnwy416@126.com>
2016-09-28 16:14:00 +0800

    ignore

--- scripts/buildup.py
+++ scripts/buildup.py
@@ -10,18 +10,24 @@ import settings
 def buildup(buildup_dir):
     """
     """
+    print 'start buildup...'
     buildup_dir = os.path.realpath(buildup_dir)
     base_dir = os.path.dirname(os.path.realpath(__file__))
 
     temp_dir = os.path.join(buildup_dir, 'temp')
+    print 'mkdir %s' % temp_dir
     if not os.path.exists(temp_dir):
         os.makedirs(temp_dir)
 
     src_dir = os.path.join(base_dir, '..')
     tempsrc_dir = os.path.join(temp_dir, 'src')
+    print 'copy src code from %s ==> %s' % (src_dir, tempsrc_dir)
     if os.path.exists(tempsrc_dir):
         shutil.rmtree(tempsrc_dir)
-    shutil.copytree(src_dir, tempsrc_dir)
+
+    def ignore(src, names):
+        return [name for name in names if name.startswith('.')]
+    shutil.copytree(src_dir, tempsrc_dir, ignore=ignore)
 
     scripts = []
     for dirpath, _, filenames in os.walk(tempsrc_dir):
@@ -39,6 +45,7 @@ def buildup(buildup_dir):
     sitepathes = pathsep.join(site.getsitepackages())
     exefilename = '%s.exe' % settings.PROJECT_NAME
     cmd = 'pyinstaller -F %s -p %s -n %s' % (script_names, sitepathes, exefilename)
+    print 'buildup ready, start cmd: %s' % cmd
 
     pwd = os.getcwd()
     os.chdir(buildup_dir)
